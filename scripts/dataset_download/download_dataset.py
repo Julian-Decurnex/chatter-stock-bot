fb0a745af75706241ebf511d9e921112d2da3b87
Julian Decurnex <j.m.decurnex@gmail.com>
2024-06-05 23:58:14 -0300

    reworked download_dataset script

--- scripts/dataset_download/download_dataset.py
+++ scripts/dataset_download/download_dataset.py
@@ -1,4 +1,3 @@
-
 import os
 import boto3
 from io import BytesIO
@@ -6,79 +5,82 @@ import PyPDF2
 from pathlib import Path
 from dotenv import load_dotenv
 
-ENV_PATH = str(Path(__file__).parent.parent / "../.env")
-load_dotenv(ENV_PATH)
+MAX_FILES_TO_DOWNLOAD = 15
 
-DATASET_ROOT_PATH = str(Path(__file__).parent.parent / "dataset_download/dataset")
-os.makedirs(DATASET_ROOT_PATH, exist_ok=True)
+# Load environment variables
+def load_environment_variables(env_path):
+    load_dotenv(env_path)
 
-AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
-AWS_BUCKET_PREFIX = os.getenv('AWS_BUCKET_PREFIX')
-AWS_ACCESS_KEY=os.getenv('AWS_ACCESS_KEY')
-AWS_SECRET_KEY=os.getenv('AWS_SECRET_KEY')
-max_files = 10
+def create_directory(path):
+    os.makedirs(path, exist_ok=True)
 
-def fetch_texts_from_s3():
-    s3 = boto3.client(
+def initialize_s3_client(aws_access_key, aws_secret_key):
+    return boto3.client(
         's3',
-        aws_access_key_id=AWS_ACCESS_KEY,
-        aws_secret_access_key=AWS_SECRET_KEY
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key
     )
 
-    response = s3.list_objects_v2(Bucket=AWS_BUCKET_NAME, Prefix=AWS_BUCKET_PREFIX)
+def fetch_pdf_list(s3_client, bucket_name, bucket_prefix):
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=bucket_prefix)
+    return [obj for obj in response.get('Contents', []) if obj['Key'].endswith('.pdf')]
 
-    texts = []
-    file_count = 0
+def download_and_extract_text(s3_client, bucket_name, obj_key, file_path):
+    pdf_data = BytesIO()
+    s3_client.download_fileobj(bucket_name, obj_key, pdf_data)
+    pdf_data.seek(0)
 
-    total_files = len([obj for obj in response['Contents'] if obj['Key'].endswith('.pdf')])
+    try:
+        pdf_reader = PyPDF2.PdfReader(pdf_data)
+        text = ''.join(page.extract_text() for page in pdf_reader.pages if page.extract_text())
+        
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(text)
+        
+        return text
+    except PyPDF2.errors.PdfReadError:
+        print(f"Failed to read the PDF file: {obj_key}. Skipping.")
+        return None
 
-    if 'Contents' in response:
-        for obj in response['Contents']:
-            if obj['Key'].endswith('.pdf'):
-                file_name = obj['Key'].split('/')[-1]
-                file_path = os.path.join(DATASET_ROOT_PATH, file_name)
+def main():
+    ENV_PATH = str(Path(__file__).parent.parent / "../.env")
+    load_environment_variables(ENV_PATH)
 
-                if os.path.exists(file_path):
-                    print(f"File {file_name} already exists. Skipping download.")
-                    file_count += 1 
-                    continue
+    DATASET_DIR = str(Path(__file__).parent.parent / "dataset_download/dataset")
+    create_directory(DATASET_DIR)
 
-                pdf_data = BytesIO()
-                s3.download_fileobj(AWS_BUCKET_NAME, obj['Key'], pdf_data)
-                pdf_data.seek(0)
+    AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
+    AWS_BUCKET_PREFIX = os.getenv('AWS_BUCKET_PREFIX')
+    AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+    AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
 
-                try:
-                    pdf_reader = PyPDF2.PdfReader(pdf_data)
-                    num_pages = len(pdf_reader.pages)
-                    text = ''
-                    for page_number in range(num_pages):
-                        page = pdf_reader.pages[page_number]
-                        try:
-                            text += page.extract_text()
-                        except TypeError:
-                            print(f"Error reading font in file: {file_name}. Skipping this page.")
-                            continue
+    s3_client = initialize_s3_client(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+    pdf_list = fetch_pdf_list(s3_client, AWS_BUCKET_NAME, AWS_BUCKET_PREFIX)
+    total_files = len(pdf_list)
+    downloaded_texts = []
+    downloaded_files_count = 0
 
-                    with open(file_path, 'w', encoding='utf-8') as f:
-                        f.write(text)
+    for pdf_obj in pdf_list:
+        file_name = pdf_obj['Key'].split('/')[-1]
+        file_path = os.path.join(DATASET_DIR, file_name)
 
-                    print(f"Downloaded file: {file_name}")
+        if os.path.exists(file_path):
+            print(f"File {file_name} already exists. Skipping download.")
+            downloaded_files_count += 1
+            continue
 
-                    texts.append(text)
-                    file_count += 1
+        text = download_and_extract_text(s3_client, AWS_BUCKET_NAME, pdf_obj['Key'], file_path)
+        if text:
+            downloaded_texts.append(text)
+            downloaded_files_count += 1
+            remaining_files = total_files - downloaded_files_count
+            print(f"Remaining files to download: {remaining_files}")
 
-                    remaining_files = total_files - file_count
-                    print(f"Remaining files to download: {remaining_files}")
+        if downloaded_files_count >= MAX_FILES_TO_DOWNLOAD:
+            break
 
-                    if file_count >= max_files:
-                        break
-                except PyPDF2.errors.PdfReadError:
-                    print(f"Error reading file: {file_name}. Skipping this file.")
-                    continue
-
-    print("All downloads have been completed.")
-
-    return texts
+    print("Download process completed.")
+    return downloaded_texts
 
 if __name__ == "__main__":
-    fetch_texts_from_s3()
+    main()
